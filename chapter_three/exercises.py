29855e6d602347d09ba44246f03b39c17965ced6
Reid Miller <r24mille@uwaterloo.ca>
2014-04-17 18:39:42 -0400

    Nearly done with exercise 3.1

--- chapter_three/exercises.py
+++ chapter_three/exercises.py
@@ -5,6 +5,7 @@ Created on Apr 17, 2014
 '''
 from chapter_two import Pmf
 from chapter_two.exercises import PmfMean
+import matplotlib.pyplot as pyplot
 
 
 def main():
@@ -12,11 +13,22 @@ def main():
     """
     cl_size_counts = {7:8, 12:8, 17:14, 22: 4, 27:6, 32:12, 37:8, 42:3, 47:2}
     
-    #Exercise 3.1
-    class_size_pmf = Pmf.MakePmfFromDict(cl_size_counts)
-    dean_mean = PmfMean(class_size_pmf)
+    # Exercise 3.1
+    cl_size_pmf = Pmf.MakePmfFromDict(cl_size_counts)
+    dean_mean = PmfMean(cl_size_pmf)
     print "Mean from the dean's perspective", dean_mean
     
+    # Sample 500 students
+    cl_size_weighted = dict.fromkeys(cl_size_counts, 0)
+    for sz, cnt in cl_size_pmf.Items():
+        weight = sz * cnt
+        cl_size_weighted[sz] = weight
+    cl_size_weighted_pmf = Pmf.MakePmfFromDict(cl_size_weighted)
+    student_mean = PmfMean(cl_size_weighted_pmf)
+    print "Mean from students' perspectives", student_mean
+    vals, probs = cl_size_weighted_pmf.Render()
+    rectangles = pyplot.bar(vals, probs)
+    pyplot.show()
 
 if __name__ == "__main__":
     main()
