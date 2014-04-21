324d286ea5493b6133ed1a681239641f1359d04b
Reid Miller <r24mille@uwaterloo.ca>
2014-04-21 12:55:20 -0400

    Completed exercise 3.1

--- chapter_three/exercises.py
+++ chapter_three/exercises.py
@@ -8,25 +8,55 @@ from chapter_two.exercises import PmfMean
 import matplotlib.pyplot as pyplot
 
 
+def StuObsPmf(d):
+    """Returns a PMF of student class size observations.
+    
+    Arguments:
+    d -- dictionary of class size:count
+    """
+    cl_size_obs = dict.fromkeys(d, 0)
+    for sz, cnt in d.iteritems():
+        freq = sz * cnt
+        cl_size_obs[sz] = freq
+    obs_hist = Pmf.MakeHistFromDict(cl_size_obs)
+    obs_pmf = Pmf.MakePmfFromHist(obs_hist)
+    return obs_pmf
+
+
+def UnbiasPmf(biased_pmf):
+    """Returns an unbiased PMF of student class sizes.
+    
+    Arguments:
+    biased_pmf -- probability mass function of observations affected by 
+                  oversampling bias
+    """
+    unbiased_pmf = biased_pmf.Copy()
+    for sz, prob in unbiased_pmf.Items():
+        unbiased_pmf.Mult(sz, 1.0 / sz)
+    unbiased_pmf.Normalize()
+    return unbiased_pmf
+        
+
 def main():
     """ Execute various exercises from chapter 3 of "Think Stats."
     """
     cl_size_counts = {7:8, 12:8, 17:14, 22: 4, 27:6, 32:12, 37:8, 42:3, 47:2}
     
     # Exercise 3.1
-    cl_size_pmf = Pmf.MakePmfFromDict(cl_size_counts)
-    dean_mean = PmfMean(cl_size_pmf)
-    print "Mean from the dean's perspective", dean_mean
+    # Internal Pmf.Normalize alters dict object, use a copy
+    cl_size_pmf = Pmf.MakePmfFromDict(cl_size_counts.copy()) 
+    print "Mean from the dean's perspective", cl_size_pmf.Mean()
     
     # Sample 500 students
-    cl_size_weighted = dict.fromkeys(cl_size_counts, 0)
-    for sz, cnt in cl_size_pmf.Items():
-        weight = sz * cnt
-        cl_size_weighted[sz] = weight
-    cl_size_weighted_pmf = Pmf.MakePmfFromDict(cl_size_weighted)
-    student_mean = PmfMean(cl_size_weighted_pmf)
-    print "Mean from students' perspectives", student_mean
-    vals, probs = cl_size_weighted_pmf.Render()
+    cl_size_obs_pmf = StuObsPmf(cl_size_counts)
+    print "Mean from student observations", cl_size_obs_pmf.Mean()
+    
+    # Unbias the observed sample
+    cl_size_obs_unbias_pmf = UnbiasPmf(cl_size_obs_pmf)
+    print "Mean from unbiased student observations", cl_size_obs_unbias_pmf.Mean()
+    
+    
+    vals, probs = cl_size_obs_unbias_pmf.Render()
     rectangles = pyplot.bar(vals, probs)
     pyplot.show()
 
