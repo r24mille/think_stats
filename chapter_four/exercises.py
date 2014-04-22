db93c84d5c4c6e7bf5ef8205be11613e50f97d69
Reid Miller <r24mille@uwaterloo.ca>
2014-04-21 22:53:26 -0400

    Placeholder files to start chapter 4.

---
+++ chapter_four/exercises.py
@@ -0,0 +1,13 @@
+'''
+Created on Apr 21, 2014
+
+@author: Reid Miller
+'''
+
+def main():
+    """Main method for executing exercises from chapter 4"""
+    
+
+
+if __name__ == '__main__':
+    main()
