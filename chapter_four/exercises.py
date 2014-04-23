36ebe66d27f768d395f1bfbd8694b48658554143
Reid Miller <r24mille@uwaterloo.ca>
2014-04-23 00:55:35 -0400

    Only halfway through exercise 4.9

--- chapter_four/exercises.py
+++ chapter_four/exercises.py
@@ -22,7 +22,7 @@ from chapter_two import thinkstats
 def paretovariate(alpha, xm):
     """Wrapper function around random.paretovariate that returns values from a
     two-parameter Pareto distribution.
-    
+
     Arguments:
     alpha -- the shape parameter
     xm -- the minimum possible value of X (ie. "x sub m")
@@ -32,14 +32,39 @@ def paretovariate(alpha, xm):
     return pareto_v
 
 
+def Sample(n=6, mu=0, sigma=1):
+    """Returns a list of n samples from a normal distribution with mean mu
+    and standard deviation sigma.
+
+    Arguments:
+    n -- list size
+    mu -- mean of normal distribution
+    sigma -- standard deviation of normal distribution
+    """
+    sample = []
+    for i in range(0, n):
+        sample.append(random.normalvariate(mu=mu, sigma=sigma))
+    return sample
+
+def Samples(k=1000):
+    """Calls Sample k times to create a list of lists
+
+    Arguments:
+    k -- number of calls to Sample
+    """
+    samples = []
+    for i in range(0, k):
+        samples.append(Sample(n=6))
+    return samples
+
 def main():
     """Main method for executing exercises from chapter 4"""
     # Exercise 4.1
     exponential_dist = []
     mu = 32.6
-    for i in range (1, 44):
-        exponential_dist.append(random.expovariate(1.0/mu))
-    exp_dist_cdf = Cdf.MakeCdfFromList(exponential_dist, 
+    for i in range (0, 44):
+        exponential_dist.append(random.expovariate(1.0 / mu))
+    exp_dist_cdf = Cdf.MakeCdfFromList(exponential_dist,
                                        name="exponential_dist")
     # scale = myplot.Cdf(exp_dist_cdf, complement=True, xscale="linear", yscale="log")
     # myplot.Show(title="Complement of an Exponential Distribution Sample", yscale=scale['yscale'])
@@ -51,7 +76,7 @@ def main():
     pareto_dist = []
     alpha = 2
     xm = 5
-    for i in range (1, 1000):
+    for i in range (0, 1000):
         pareto_dist.append(paretovariate(alpha, xm))
     pareto_dist_cdf = Cdf.MakeCdfFromList(pareto_dist, name="pareto_dist")
     # scale = myplot.Cdf(pareto_dist_cdf, complement=True, transform="pareto")
@@ -62,31 +87,31 @@ def main():
     pareto_heights = []
     alpha = 1.7
     xm = 100
-    for i in range (1, 6000):
+    for i in range (0, 6000):
         pareto_heights.append(paretovariate(alpha, xm))
-    pareto_dist_cdf = Cdf.MakeCdfFromList(pareto_heights, 
+    pareto_dist_cdf = Cdf.MakeCdfFromList(pareto_heights,
                                           name="pareto_heights")
     print "Mean height in Pareto World", pareto_dist_cdf.Mean()
     print "Max height in Pareto World", max(pareto_dist_cdf.Values())
     
     # Exercise 4.5
-    filename = os.path.join(".", "kjvdat.txt.gz") # King James Bible
+    filename = os.path.join(".", "kjvdat.txt.gz")  # King James Bible
     fp = gzip.open(filename)
     counts = collections.Counter()
-    words_re = re.compile("(\w[\w']*)") # Simple word-split regex
+    words_re = re.compile("(\w[\w']*)")  # Simple word-split regex
     # Iterate over lines in text file
     for i, line in enumerate(fp):
         if i == None:
             break
         counts.update(words_re.findall(line.lower()))
     fp.close()
-    bible_words_cdf = Cdf.MakeCdfFromList(counts.values(), 
+    bible_words_cdf = Cdf.MakeCdfFromList(counts.values(),
                                           name="bible_words_cdf")
     # scale = myplot.Cdf(bible_words_cdf, complement=True, transform="pareto")
     # myplot.Show(title="Complement of Word Occurences in KJV Bible",
     #             yscale=scale["yscale"], xscale=scale["xscale"])
     xs, ps = bible_words_cdf.Render()
-    ps = [1.0-p for p in ps] # complement
+    ps = [1.0 - p for p in ps]  # complement
     del xs[-1]
     del ps[-1]
     slope, intercept = numpy.polyfit(numpy.log(xs), numpy.log(ps), 1)
@@ -95,9 +120,9 @@ def main():
     
     # Exercise 4.6
     weibull_dist = []
-    for i in range(1, 1000):
+    for i in range(0, 1000):
         weibull_dist.append(random.weibullvariate(alpha=1, beta=1))
-    weibull_dist_cdf = Cdf.MakeCdfFromList(weibull_dist, 
+    weibull_dist_cdf = Cdf.MakeCdfFromList(weibull_dist,
                                        name="weibull_dist")
     # scale = myplot.Cdf(weibull_dist_cdf, complement=True, yscale="log")
     # myplot.Show(title="Weibull Distribution Sample", yscale=scale["yscale"])
@@ -117,7 +142,7 @@ def main():
     len_var = thinkstats.Var(table.lengths)
     len_sigma = math.sqrt(len_var)
     model_lengths = []
-    for i in range(1, 1000):
+    for i in range(0, 1000):
         model_lengths.append(random.gauss(mu=table.mu, sigma=len_sigma))
     normal_cdf = Cdf.MakeCdfFromList(model_lengths, name="gaussian_model")
     # myplot.Cdf(lengths_cdf)
@@ -125,7 +150,9 @@ def main():
     # myplot.Show(title="Empirical and Modeled Gestation Lengths")
     
     # Exercise 4.9
-    
+    samples = Samples()
+    z_samples = zip(samples)
+    print "Zipped length", len(z_samples)
 
 if __name__ == '__main__':
     main()
