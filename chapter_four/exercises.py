d95102776eb5fed0f85baddff755149f62f82324
Reid Miller <r24mille@uwaterloo.ca>
2014-04-24 18:31:23 -0400

    Completed chapter 4 exercises.

--- chapter_four/exercises.py
+++ chapter_four/exercises.py
@@ -6,14 +6,21 @@ Created on Apr 21, 2014
 import collections
 import gzip
 import math
-import numpy
 import os
 import random
 import re
 
+from matplotlib import pyplot
+import matplotlib.pyplot
+import numpy
+
+from chapter_four import brfss
 from chapter_four import erf
+from chapter_four import irs
+from chapter_four import populations
 from chapter_one import first_ans as first
 from chapter_three import Cdf
+from chapter_three import relay
 from chapter_two import Pmf
 from chapter_two import myplot
 from chapter_two import thinkstats
@@ -32,6 +39,22 @@ def paretovariate(alpha, xm):
     return pareto_v
 
 
+def NormalPlot(seq, ylabel="", title="Normal Rankit"):
+    """Take a sequence of values and generate a normal probability plot.
+    
+    Arguments:
+    seq -- list of values
+    ylabel -- y-axis label
+    """
+    seq.sort()
+    sample = Sample(n=len(seq))
+    pyplot.plot(sample, seq)
+    pyplot.ylabel(ylabel)
+    pyplot.xlabel("Normal Distribution Values")
+    pyplot.title(title)
+    pyplot.show()
+
+
 def Sample(n=6, mu=0, sigma=1):
     """Returns a list of n samples from a normal distribution with mean mu
     and standard deviation sigma.
@@ -43,9 +66,26 @@ def Sample(n=6, mu=0, sigma=1):
     """
     sample = []
     for i in range(0, n):
-        sample.append(random.normalvariate(mu=0, sigma=1))
+        sample.append(random.normalvariate(mu=mu, sigma=sigma))
     sample.sort()
     return sample
+
+
+def LogNormSample(n=6, mu=0, sigma=1):
+    """Returns a list of n samples from a lognormal distribution with mean mu
+    and standard deviation sigma.
+    
+    Arguments:
+    n -- list size
+    mu -- mean of lognormal distribution
+    sigma -- standard deviation of lognormal distribution
+    """
+    sample = []
+    for i in range(0, n):
+        sample.append(random.lognormvariate(mu=mu, sigma=sigma))
+    sample.sort()
+    return sample
+
 
 def Samples(k=1000):
     """Calls Sample k times to create a list of lists
@@ -57,6 +97,20 @@ def Samples(k=1000):
     for i in range(0, k):
         samples.append(Sample(n=6))
     return samples
+
+
+def weibullvariate(lam, k):
+    """Returns a random value from the Weivull distribution according to 
+    parameters.
+    
+    Arguments:
+    lam -- Lamda, which controls the shape of the distribution
+    k -- Used as exponent in cumulative distribution function
+    """
+    p = random.random()
+    x = (lam * (-math.log(1-p)))**(1/k)
+    return x
+
 
 def main():
     """Main method for executing exercises from chapter 4"""
@@ -125,8 +179,8 @@ def main():
         weibull_dist.append(random.weibullvariate(alpha=1, beta=1))
     weibull_dist_cdf = Cdf.MakeCdfFromList(weibull_dist,
                                        name="weibull_dist")
-    # scale = myplot.Cdf(weibull_dist_cdf, complement=True, yscale="log")
-    # myplot.Show(title="Weibull Distribution Sample", yscale=scale["yscale"])
+    scale = myplot.Cdf(weibull_dist_cdf, complement=True, yscale="log")
+    myplot.Show(title="Weibull Distribution Sample", yscale=scale["yscale"])
     
     # Exercise 4.7
     iqs = [115, 130, 145]
@@ -153,10 +207,56 @@ def main():
     # Exercise 4.9
     samples = Samples(k=1000)
     z_samples = zip(*samples)
-    # print "zipped samples", z_samples
-    # for s in z_samples:
-    #     print "s", s
     print "Zipped means", [ thinkstats.Mean(s) for s in z_samples ]
+    
+    # Exercise 4.10
+    results = relay.ReadResults()
+    speeds = relay.GetSpeeds(results)
+    # NormalPlot(speeds, ylabel="Marathon Mile Pace (minutes)")
+    
+    # Exercise 4.11
+    # respondents = brfss.Respondents()
+    # respondents.ReadRecords()
+    # weights = [record.weight2 for record in respondents.records]
+    # weights = list(filter(("NA").__ne__, weights))
+    # NormalPlot(weights, ylabel="Adult Weights (lbs)", title="Normal Rankit")
+    # NormalPlot([math.log(x) for x in weights], 
+    #            ylabel="Log of Adult Weights (lbs)", 
+    #            title="Lognormal Rankit")
+    
+    # Exercise 4.12
+    # pops = populations.ReadData()
+    # pops_cdf = Cdf.MakeCdfFromList(pops, name="population")
+    # myplot.Cdf(pops_cdf, complement=False)
+    # myplot.Show(title="City Populations")
+    # scale = myplot.Cdf(pops_cdf, complement=False, xscale="log")
+    # myplot.Show(title="City Populations, x-scale=log", xscale=scale["xscale"])
+    # scale = myplot.Cdf(pops_cdf, transform="pareto")
+    # myplot.Show(title="City Populations, Pareto Distribution", 
+    #             xscale=scale["xscale"],
+    #             yscale=scale["yscale"])
+    # scale = myplot.Cdf(pops_cdf, transform="weibull")
+    # myplot.Show(title="City Populations, Weibull Distribution", 
+    #             xscale=scale["xscale"],
+    #             yscale=scale["yscale"])
+    
+    # Exercise 4.13
+    # incomes = irs.ReadIncomeFile()
+    # income_hist, income_pmf, income_cdf = irs.MakeIncomeDist(incomes)
+    # scale = myplot.Cdf(income_cdf, transform="pareto")
+    # myplot.Show(title="Incomes",
+    #             xscale=scale["xscale"],
+    #             yscale=scale["yscale"])
+    
+    # Exercise 4.14
+    weibull_dist = []
+    for i in range(0, 1000):
+        weibull_dist.append(weibullvariate(k=1, lam=1))
+    weibull_dist_cdf = Cdf.MakeCdfFromList(weibull_dist,
+                                       name="weibull_dist")
+    scale = myplot.Cdf(weibull_dist_cdf, complement=True, yscale="log")
+    myplot.Show(title="Weibull Distribution Sample from My Generator", 
+                yscale=scale["yscale"])
 
 if __name__ == '__main__':
     main()
